ce3ee312f9cdc9aadf54be956fe88ee0512236e9
DadaKhalandar Bukkapatnam <bababudan1998@gmail.com>
2020-04-24 17:06:24 +0530

    KeyRead Java program added

---
+++ KeyRead_Line2Runner.java
@@ -0,0 +1,11 @@
+class KeyRead_Line2Runner
+{
+    public static void main(String[] args) {
+        KeyRead_Line2 kr = new KeyRead_Line2();
+        kr.read();
+        kr.display();
+        kr.sortedArray();
+        kr.display();
+        kr.search();
+    }
+}
